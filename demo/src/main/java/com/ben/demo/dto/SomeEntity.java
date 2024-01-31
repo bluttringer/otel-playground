1af27c67eef1925e004f49f503a8d3764a48b864
Benoit Luttringer <bluttringer@solocal.com>
2024-01-31 17:20:13 +0100

    Delete demo

--- demo/src/main/java/com/ben/demo/dto/SomeEntity.java
+++
@@ -1,9 +0,0 @@
-package com.ben.demo.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-@Builder
-public record SomeEntity(String name, String description) {
-}
