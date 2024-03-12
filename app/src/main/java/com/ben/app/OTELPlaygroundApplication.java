c57cd9b90559f9ea514c89dcb9d9763ed31e1d27
Benoit Luttringer <bluttringer@solocal.com>
2024-03-12 17:06:08 +0100

    Typo in application.properties caused micrometer to not send data to otel-collector ...

--- app/src/main/java/com/ben/app/OTELPlaygroundApplication.java
+++ app/src/main/java/com/ben/app/OTELPlaygroundApplication.java
@@ -1,7 +1,13 @@
 package com.ben.app;
 
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporterBuilder;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.tracing.otlp.OtlpProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class OTELPlaygroundApplication {
@@ -10,4 +16,17 @@ public class OTELPlaygroundApplication {
 		SpringApplication.run(OTELPlaygroundApplication.class, args);
 	}
 
+//	@Bean
+//	OtlpHttpSpanExporter otlpHttpSpanExporter(OtlpProperties properties) {
+//		OtlpHttpSpanExporterBuilder builder = OtlpHttpSpanExporter.builder()
+//				.setEndpoint(properties.getEndpoint())
+//				.setTimeout(properties.getTimeout());
+//
+//		for (Map.Entry<String, String> header : properties.getHeaders().entrySet()) {
+//			builder.addHeader(header.getKey(), header.getValue());
+//		}
+//
+//		return builder.build();
+//
+//	}
 }
