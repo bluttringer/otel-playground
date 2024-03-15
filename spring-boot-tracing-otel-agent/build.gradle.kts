b9203060424f826fbac452019b7491f9bbcf068a
Benoit Luttringer <bluttringer@solocal.com>
2024-03-15 17:05:43 +0100

    Adding otel java agent instrumentation example

--- spring-boot-tracing-otel-agent/build.gradle.kts
+++ spring-boot-tracing-otel-agent/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("com.ryandens.javaagent-application") version "0.4.2"
 }
 
 group = "com.ben"
@@ -19,6 +20,11 @@ configurations {
 	}
 }
 
+application {
+	mainClass = "com.ben.app.OTELPlaygroundApplication"
+	applicationDefaultJvmArgs = listOf("-Dotel.javaagent.configuration-file=src/main/resources/otel-agent.properties")
+}
+
 repositories {
 	mavenCentral()
 }
@@ -26,12 +32,12 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("io.micrometer:micrometer-tracing-bridge-otel")
 	implementation("io.opentelemetry:opentelemetry-exporter-otlp")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	javaagent("io.opentelemetry.javaagent:opentelemetry-javaagent:2.2.0")
 }
 
 tasks.withType<Test> {
--- spring-boot-tracing-otel-agent/src/main/java/com/ben/app/web/DemoController.java
+++ spring-boot-tracing-otel-agent/src/main/java/com/ben/app/web/DemoController.java
@@ -1,13 +1,6 @@
 package com.ben.app.web;
 
 import com.ben.app.dto.SomeEntity;
-import io.micrometer.observation.Observation;
-import io.micrometer.observation.ObservationHandler;
-import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.tracing.Span;
-import io.micrometer.tracing.handler.DefaultTracingObservationHandler;
-import io.micrometer.tracing.handler.PropagatingReceiverTracingObservationHandler;
-import io.micrometer.tracing.handler.PropagatingSenderTracingObservationHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
