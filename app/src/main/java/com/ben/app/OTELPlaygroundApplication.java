67fac6dc6c0d9c324902dcc8b4be0c5585f4101d
Benoit Luttringer <bluttringer@solocal.com>
2024-03-15 15:27:41 +0100

    Cleaning

--- app/src/main/java/com/ben/app/OTELPlaygroundApplication.java
+++ app/src/main/java/com/ben/app/OTELPlaygroundApplication.java
@@ -1,32 +1,13 @@
 package com.ben.app;
 
-import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
-import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporterBuilder;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.tracing.otlp.OtlpProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Map;
 
 @SpringBootApplication
 public class OTELPlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OTELPlaygroundApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OTELPlaygroundApplication.class, args);
+    }
 
-//	@Bean
-//	OtlpHttpSpanExporter otlpHttpSpanExporter(OtlpProperties properties) {
-//		OtlpHttpSpanExporterBuilder builder = OtlpHttpSpanExporter.builder()
-//				.setEndpoint(properties.getEndpoint())
-//				.setTimeout(properties.getTimeout());
-//
-//		for (Map.Entry<String, String> header : properties.getHeaders().entrySet()) {
-//			builder.addHeader(header.getKey(), header.getValue());
-//		}
-//
-//		return builder.build();
-//
-//	}
 }
